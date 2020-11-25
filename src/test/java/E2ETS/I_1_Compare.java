2b105524ac196ab729dfb0f0ec303518239a5624
f.michel <f.michiel@z2data.com>
2020-11-25 08:35:38 +0200

    PR25112020:8.35Am

--- src/test/java/E2ETS/I_1_Compare.java
+++ src/test/java/E2ETS/I_1_Compare.java
@@ -5,6 +5,7 @@ import Com.PartRisk.Pages.Landing_Page;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -27,12 +28,30 @@ public class I_1_Compare extends Test_Base {
         Wait_Element_Visible(CompareObj.Compare1);
         CompareObj.Z2D_Input_Compare_1(Z2DataCompare_1);
         Wait_Element_Clickable(CompareObj.Value_Compare1);
-        act.moveToElement(CompareObj.Value_Compare1).click().build().perform();
+        boolean staleElement = true;
+        while (staleElement) {
+            try {
+                act.moveToElement(CompareObj.Value_Compare1).click().build().perform();;
+                staleElement = false;
+            } catch (StaleElementReferenceException e) {
+                staleElement = true;
+            }
+        }
+
         Wait_Element_Visible(CompareObj.Compare2);
         CompareObj.Z2D_Input_Compare_2(Z2DataCompare_2);
         Wait_Element_Clickable(CompareObj.Value_Compare2);
-        act.moveToElement(CompareObj.Value_Compare2).click().build().perform();
-        Wait_Element_Clickable(CompareObj.Differences);
+        boolean staleElement1 = true;
+        while (staleElement1) {
+            try {
+                act.moveToElement(CompareObj.Value_Compare2).click().build().perform();;
+                staleElement1 = false;
+            } catch (StaleElementReferenceException e) {
+                staleElement1 = true;
+            }
+        }
+
+        Wait_Element_Visible(CompareObj.Differences);
         CompareObj.Z2D_Select_Differences();
         CompareObj.Z2D_Select_Similarities();
     }
