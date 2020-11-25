65e09678ae2f117eced53c4fc9b3adb289ef95d3
f.michel <f.michiel@z2data.com>
2020-11-25 09:27:18 +0200

    PR25112020:8.35Am

--- src/test/java/E2ETS/Test_Base.java
+++ src/test/java/E2ETS/Test_Base.java
@@ -50,9 +50,9 @@ public class Test_Base {
         ChromeOptions options = new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         options.setProxy(null);
-        //options.addArguments("--headless");
-        // options.addArguments("--disable-gpu");
-        // options.addArguments("--window-size=1400,800");
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1400,800");
         options.addArguments("--disable-remote-fonts");
         options.addArguments("--enable-precache");
         options.addArguments("--start-maximized");
