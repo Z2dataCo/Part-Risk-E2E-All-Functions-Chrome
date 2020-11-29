e2ff73a7c2c42b94f4d54a975fe0c1ffe5e0e3e1
zayatoo <ahmedzayat2311@gmail.com>
2020-11-29 11:46:47 +0200

    AZ29112020|11:46AM Updating New Sc Ae Final

--- src/test/java/E2ETS/AE_2_Fun_Check_Smelters_Report.java
+++ src/test/java/E2ETS/AE_2_Fun_Check_Smelters_Report.java
@@ -24,9 +24,7 @@ public class AE_2_Fun_Check_Smelters_Report extends Test_Base{
         DManagementObj.Z2D_Move_to_Reports_Bom(driver);
         ReportPageObj.Z2D_Open_Smelters_Report();
         Wait_Element_Visible(ReportPageObj.Smelters_Window_Title);
-        String Title = ReportPageObj.Smelters_Window_Title.getText();
-        System.out.println(Title);
-        Assert.assertTrue(Title.contains("Conflict Mineral Compliance by Smelter"));
+        Assert.assertTrue(ReportPageObj.Smelters_Window_Title.getText().contains("Conflict Mineral Compliance by Smelter"));
         Wait_Element_Visible(ReportPageObj.Export_Button);
         ReportPageObj.Z2D_Export_Report_Excel();
     }
