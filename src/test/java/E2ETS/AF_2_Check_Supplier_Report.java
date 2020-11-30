8515ee9c42c02d8a68649772cff801c410fc890b
zayatoo <ahmedzayat2311@gmail.com>
2020-11-30 09:57:35 +0200

    AZ30112020|9:56AM Updating Af

--- src/test/java/E2ETS/AF_2_Check_Supplier_Report.java
+++ src/test/java/E2ETS/AF_2_Check_Supplier_Report.java
@@ -23,43 +23,33 @@ public class AF_2_Check_Supplier_Report extends Test_Base{
 
         DManagementObj.Z2D_Move_to_Reports_Bom(driver);
         ReportPageObj.Z2D_Open_Supplier_Report();
-
         Wait_Element_Invisibility(LandObj.SpinnerZezo);
         Wait_Element_Visible(ReportPageObj.Smelters_Window_Title);
         Assert.assertTrue(ReportPageObj.Smelters_Window_Title.getText().contains("Conflict Mineral Compliance by Supplier"));
 
-        String Total = ReportPageObj.First_Filter.getText();
-        System.out.println(Total);
-        Wait_Text_Not_To_be(ReportPageObj.Info_Total,"0");
-        String Info_Total=ReportPageObj.Info_Total.getText();
-        System.out.println(Info_Total);
-        Assert.assertTrue(Total.contains(Info_Total));
+        String First_Filter = ReportPageObj.First_Filter.getText();
+        Wait_Text_Not_To_be(ReportPageObj.Info_Total,First_Filter.substring(0,2));
+        Assert.assertTrue(First_Filter.contains(ReportPageObj.Info_Total.getText()));
 
+        Wait_Element_Invisibility(LandObj.SpinnerZezo);
+        Wait_Element_Clickable(ReportPageObj.Second_Filter);
         ReportPageObj.Z2D_Click_on_2nd_Filter();
-        Wait_Element_Invisibility(LandObj.SpinnerZezo);
         String Sec_Filter = ReportPageObj.Second_Filter.getText();
-        System.out.println(Sec_Filter);
-        Wait_Text_Not_To_be(ReportPageObj.Info_Total,"0");
-        String Info_Total2=ReportPageObj.Info_Total.getText();
-        System.out.println(Info_Total2);
-        Assert.assertTrue(Sec_Filter.contains(Info_Total2));
+        Wait_Text_To_be(ReportPageObj.Info_Total,Sec_Filter.substring(0,2));
+        Assert.assertTrue(Sec_Filter.contains(ReportPageObj.Info_Total.getText()));
 
+        Wait_Element_Invisibility(LandObj.SpinnerZezo);
+        Wait_Element_Clickable(ReportPageObj.Third_Filter);
         ReportPageObj.Z2D_Click_on_3rd_Filter();
-        Wait_Element_Invisibility(LandObj.SpinnerZezo);
         String Third_Filter = ReportPageObj.Third_Filter.getText();
-        System.out.println(Third_Filter);
-        Wait_Text_Not_To_be(ReportPageObj.Info_Total,"0");
-        String Info_Total3 = ReportPageObj.Info_Total.getText();
-        System.out.println(Info_Total3);
-        Assert.assertTrue(Third_Filter.contains(Info_Total3));
+        Wait_Text_To_be(ReportPageObj.Info_Total,Third_Filter.substring(0,2));
+        Assert.assertTrue(Third_Filter.contains(ReportPageObj.Info_Total.getText()));
 
-        ReportPageObj.Z2D_Click_on_4th_Filter();
         Wait_Element_Invisibility(LandObj.SpinnerZezo);
+        Wait_Element_Clickable(ReportPageObj.Fourth_Filter);
+        ReportPageObj.Z2D_Click_on_4th_Filter();
         String Fourth_Filter = ReportPageObj.Fourth_Filter.getText();
-        System.out.println(Fourth_Filter);
-        Wait_Text_To_be(ReportPageObj.Info_Total,"50");
-        String Info_Total4 = ReportPageObj.Info_Total.getText();
-        System.out.println(Info_Total4);
-        Assert.assertTrue(Fourth_Filter.contains(Info_Total4));
+        Wait_Text_To_be(ReportPageObj.Info_Total,Fourth_Filter.substring(0,2));
+        Assert.assertTrue(Fourth_Filter.contains(ReportPageObj.Info_Total.getText()));
     }
     }
